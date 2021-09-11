e92bec3ae286879f4f6956360e658d73be850868
CrysD5 <Matrim.ivy@gmail.com>
2021-09-11 18:14:59 -0400

    completed and successfully run all four codes

--- LoopsExercises/Program.cs
+++ LoopsExercises/Program.cs
@@ -52,6 +52,13 @@ namespace LoopsExercises
             Console.WriteLine("PRINT NUMBERS IN REVERSE ORDER\n");
 
             // Enter your solution here
+           
+
+            for(int i = 5; i >= 1; i--)
+            {
+                Console.WriteLine(i);
+                
+            }
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
@@ -68,6 +75,24 @@ namespace LoopsExercises
 
             // Enter your solution here
 
+            Console.WriteLine("Enter a number between 1-20");
+
+            int a = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("I will count down to 1 from the number you entered: {0} \n", a);
+
+            while (a > 0)
+
+            {
+                
+                Console.WriteLine(a);
+
+                a--;
+
+            }
+
+
+
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
         }
@@ -80,6 +105,17 @@ namespace LoopsExercises
             Console.WriteLine("PRINT THE SQUARE OF EVEN NUMBERS\n");
 
             // Enter your solution here
+            for (int n = 1; n < (9 + 1); n++)
+            {
+                if(n % 2 == 0)
+                {
+                    Console.WriteLine(n * n);
+                }
+                else
+                {
+                    Console.WriteLine(n);
+                }
+            }
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
@@ -93,10 +129,33 @@ namespace LoopsExercises
             // Keep asking them for a number (looping) until they win.
 
             Console.Clear();
-            Console.WriteLine("GUESS THE MAGIC NUMBER\n");
+           // Console.WriteLine("GUESS THE MAGIC NUMBER\n");
 
-            int userInput = Convert.ToInt32(Console.ReadLine()); // This reads the user's input and converts it to an integer. We'll assume that users can only input integers.
+            //int userInput = Convert.ToInt32(Console.ReadLine()); // This reads the user's input and converts it to an integer. We'll assume that users can only input integers.
             // Enter your solution here
+
+          
+            bool hasWon = false;
+
+            while(hasWon == false)
+            {
+                //get user guess
+                Console.WriteLine("Please enter a number: ");
+                int guess = Convert.ToInt32(Console.ReadLine());
+
+                if((guess%3) == 0)
+                {
+                    //meet winning condition
+                    hasWon = true;
+                    Console.WriteLine("You Won!");
+                }
+                else
+                {
+                    hasWon = false;
+                    Console.WriteLine("Try again.");
+                }
+
+            }
 
             Console.Write("Press enter to return to the Main Menu");
             Console.ReadLine();
